2e4bc84bfdb911f65f1746fdf7508cfab9543bfc
notoliver <oliver.qiu@hotmail.com>
2022-05-23 15:01:29 -0400

    extra music

--- music/loadmusic.js
+++ music/loadmusic.js
@@ -1,26 +1,49 @@
 var releases = [
 	{
         url: 'ep5',
+        img: 'ep5',
         title: 'More Things',
         date: 'November 21, 2021'
     },
     {
+        url: 'https://veryezvery.fun/yetso/',
+        img: 'yetso',
+        title: 'Yet So (All of Hers)',
+        date: 'November 12, 2021'
+    },
+    {
+        url: 'https://theopenness.com/turnaround/',
+        img: 'turnaround',
+        title: 'Television Turnaround (Openness)',
+        date:'April 30, 2021'
+    },
+    {
         url: 'ep4',
+        img: 'ep4',
         title: 'Compact Muzak',
         date:'February 19, 2021'
     },
+	{
+        url: 'https://theopenness.com/journey/',
+        img: 'adorethejourney',
+        title: 'Adore the Journey (Openness)',
+        date:'August 21, 2020'
+    },
     {
         url: 'ep3',
+        img: 'ep3',
         title: 'Sounds For Being: Sitting',
         date: 'July 30, 2020'
     },
     {
         url: 'ep2',
+        img: 'ep2',
         title: 'Sounds For Being: Lying',
         date: 'April 22, 2020'
     },
     {
         url: 'ep1',
+        img: 'ep1',
         title: 'Some Things',
         date: 'February 21, 2020'
     },
@@ -45,11 +68,8 @@ for(var i=0;i<releases.length;i++){
     if(releases[i].url == 'empty'){
         innertext += '<figure><\/figure>'
     }
-	else if(releases[i].url == 'TEMP'){
-        innertext += '<figure>\r\n\r\n        <img src=\'ep5.jpg\' alt=\'ep4\'\/>\r\n        <div class=\'caption\'>More Things<\/div>\r\n        <div class=\'caption date\'>Coming November 21, 2021<\/div>\r\n\r\n<\/figure>'
-    }
     else{
-        innertext += '<figure>\r\n    <a href=\''+releases[i].url+'\'>\r\n        <img src=\''+releases[i].url+'.jpg\' alt=\''+releases[i].url+'\'\/>\r\n        <div class=\'caption\'>'+releases[i].title+'<\/div>\r\n        <div class=\'caption date\'>'+releases[i].date+'<\/div>\r\n    <\/a>\r\n<\/figure>'
+        innertext += '<figure>\r\n<a href=\''+releases[i].url+'\'>\r\n<img src=\''+releases[i].img+'.jpg\' alt=\''+releases[i].url+'\'\/>\r\n<div class=\'caption\'>'+releases[i].title+'<\/div>\r\n<div class=\'caption date\'>'+releases[i].date+'<\/div>\r\n<\/a>\r\n<\/figure>'
     }
 }
 
